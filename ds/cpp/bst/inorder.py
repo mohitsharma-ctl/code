e16ea305ef845c48a114ed5c7e7d704153dc5c76
ms1241721 <55950356+ms1241721@users.noreply.github.com>
2022-02-05 19:53:46 +0530

    bst traversal

--- ds/cpp/bst/inorder.py
+++ ds/cpp/bst/inorder.py
@@ -1,17 +1,16 @@
-def inorderTraversal(root):
+ def inorderTraversal(root):
     if root==None: return None
     stack   =  [root]
     counter =  [1]      #created additional stack to map the counter
     output = []
     while(len(stack)>0):
         if counter[-1]==1:     #if counter of top of stack is 1 , its a first time visit, means preorder
-             output.append(stack[-1].val)
             counter[-1]+=1
             if stack[-1].left !=None:
                 stack.append(stack[-1].left)
                 counter.append(1)
         elif counter[-1] ==2:    #if counter of top of stack is 2 , its a second time visit, means inorder
-           
+            output.append(stack[-1].val)
             counter[-1]+=1
             if stack[-1].right !=None:   
                 stack.append(stack[-1].right)
---
+++ ds/cpp/bst/postorder.py
@@ -0,0 +1,22 @@
+ def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root==None: return None
+        stack   =  [root]
+        counter =  [1]      #created additional stack to map the counter
+        output = []
+        while(len(stack)>0):
+            if counter[-1]==1:     #if counter of top of stack is 1 , its a first time visit, means preorder
+                counter[-1]+=1
+                if stack[-1].left !=None:
+                    stack.append(stack[-1].left)
+                    counter.append(1)
+            elif counter[-1] ==2:    #if counter of top of stack is 2 , its a second time visit, means inorder
+                
+                counter[-1]+=1
+                if stack[-1].right !=None:   
+                    stack.append(stack[-1].right)
+                    counter.append(1)
+            elif counter[-1] ==3:   #if counter of top of stack is 3 , its a third time visit, means postorder
+                output.append(stack[-1].val)
+                stack.pop()
+                counter.pop()
+        return output
