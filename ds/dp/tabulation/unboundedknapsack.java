453b0410e42bd48654b2c57e7d2711e1fb3b9ead
ms1241721 <55950356+ms1241721@users.noreply.github.com>
2022-04-07 16:50:41 +0530

    unboundedknapsack

---
+++ ds/dp/tabulation/unboundedknapsack.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    
+    int[] weight = {2,5,1,3,4};
+    int[] value = {15,14,10,45,30};
+    int total = 7;
+    int[][]dp = new int[weight.length+1][total+1];
+    
+
+  for (int i = 0; i < dp.length; i++) 
+        {
+            for (int j = 0; j < dp[0].length; j++) 
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+
+                else if (j>= weight[i - 1])
+                
+                    dp[i][j]
+                        = Math.max(value[i - 1]
+                         + dp[i][j - weight[i - 1]],          
+                         dp[i - 1][j]);
+// max(person without bat, person with bat and with remaining ball if he can play)
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+    for(int i = 0;i<dp.length;i++){
+      System.out.println("");
+      // 
+      for(int j = 0;j<dp[0].length;j++){
+      // 
+      System.out.print(dp[i][j]+" ");
+    // 
+      }
+      // 
+      // 
+    }
+  
+    }
+}
